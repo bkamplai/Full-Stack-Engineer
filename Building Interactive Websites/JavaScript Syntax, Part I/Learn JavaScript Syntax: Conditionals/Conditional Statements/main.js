61818872f5d13cc2b640d203db28dd266c200184
bkamplai <sterypti@gmail.com>
2024-07-05 17:50:35 -0600

    Truthy and Falsy

--- Building Interactive Websites/JavaScript Syntax, Part I/Learn JavaScript Syntax: Conditionals/Conditional Statements/main.js
+++ Building Interactive Websites/JavaScript Syntax, Part I/Learn JavaScript Syntax: Conditionals/Conditional Statements/main.js
@@ -20,3 +20,16 @@ if (mood === 'sleepy' && tirednessLevel > 8) {
 } else {
     console.log('not bed time yet');
 }
+
+let wordCount = 1;
+if (wordCount) {
+    console.log("Great! You've started your work!");
+} else {
+    console.log('Better get to work!');
+}
+let favoritePhrase = '';
+if (favoritePhrase) {
+    console.log("This string doesn't seem to be empty.");
+} else {
+    console.log('This string is definitely empty.');
+}

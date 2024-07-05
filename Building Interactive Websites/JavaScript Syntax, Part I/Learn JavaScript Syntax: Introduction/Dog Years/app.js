346f3e9c41ef1db6320921077cb2f5c54d5f6a85
bkamplai <sterypti@gmail.com>
2024-07-05 17:32:25 -0600

    Dog Years

---
+++ Building Interactive Websites/JavaScript Syntax, Part I/Learn JavaScript Syntax: Introduction/Dog Years/app.js
@@ -0,0 +1,10 @@
+const myAge = 22;
+let earlyYears = 2;
+earlyYears *= 10.5;
+let laterYears = myAge - 2;
+laterYears = laterYears * 4;
+// console.log(earlyYears);
+// console.log(laterYears);
+let myAgeInDogYears = earlyYears + laterYears;
+const myName = 'Brandon'.toLowerCase();
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);

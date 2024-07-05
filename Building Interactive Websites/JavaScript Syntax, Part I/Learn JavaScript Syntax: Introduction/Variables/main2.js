4804a6e99aff92b2f0083a5f99d9b183f2e184ed
bkamplai <sterypti@gmail.com>
2024-07-05 16:13:02 -0600

    Create a Variable: const

---
+++ Building Interactive Websites/JavaScript Syntax, Part I/Learn JavaScript Syntax: Introduction/Variables/main2.js
@@ -0,0 +1,4 @@
+const entree = 'Enchiladas';
+console.log(entree);
+// entree = 'Tacos'; TypeError: Assignment to constant variable.
+// const testing; SyntaxError: Missing initializer in const declaration

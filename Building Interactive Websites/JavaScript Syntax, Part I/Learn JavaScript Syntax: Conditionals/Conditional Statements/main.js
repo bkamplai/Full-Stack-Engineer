710676144e2d733e71b7a5b17c2b45c889da79e4
bkamplai <sterypti@gmail.com>
2024-07-05 17:48:39 -0600

    Logical Operators

--- Building Interactive Websites/JavaScript Syntax, Part I/Learn JavaScript Syntax: Conditionals/Conditional Statements/main.js
+++ Building Interactive Websites/JavaScript Syntax, Part I/Learn JavaScript Syntax: Conditionals/Conditional Statements/main.js
@@ -12,3 +12,11 @@ if (hungerLevel > 7) {
 } else {
     console.log('We can eat later!');
 }
+
+let mood = 'sleepy';
+let tirednessLevel = 6;
+if (mood === 'sleepy' && tirednessLevel > 8) {
+    console.log('time to sleep');
+} else {
+    console.log('not bed time yet');
+}

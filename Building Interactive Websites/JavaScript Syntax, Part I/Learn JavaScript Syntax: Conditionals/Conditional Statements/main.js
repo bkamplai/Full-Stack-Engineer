86e05d700b133a72784f4fb915f53fba2e475ccf
bkamplai <sterypti@gmail.com>
2024-07-05 17:46:26 -0600

    Comparison Operators

--- Building Interactive Websites/JavaScript Syntax, Part I/Learn JavaScript Syntax: Conditionals/Conditional Statements/main.js
+++ Building Interactive Websites/JavaScript Syntax, Part I/Learn JavaScript Syntax: Conditionals/Conditional Statements/main.js
@@ -5,3 +5,10 @@ if (sale) {
 } else {
     console.log('Time to wait for a sale.');
 }
+
+let hungerLevel = 7;
+if (hungerLevel > 7) {
+    console.log('Time to eat!');
+} else {
+    console.log('We can eat later!');
+}

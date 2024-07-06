1d44365f0f91d7c42c7c93b034951d7aae7fa0bc
bkamplai <sterypti@gmail.com>
2024-07-06 12:47:17 -0600

    Default Parameters

--- Building Interactive Websites/JavaScript Syntax, Part I/Learn JavaScript Syntax: Functions/Functions/main.js
+++ Building Interactive Websites/JavaScript Syntax, Part I/Learn JavaScript Syntax: Functions/Functions/main.js
@@ -16,3 +16,10 @@ function sayThanks(name) {
 sayThanks('Cole');
 sayThanks('Cole');
 sayThanks('Cole');
+
+// Default Parameters
+function makeShoppingList(item1 = 'milk', item2 = 'bread', item3 = 'eggs') {
+    console.log(`Remember to buy ${item1}`);
+    console.log(`Remember to buy ${item2}`);
+    console.log(`Remember to buy ${item3}`);
+}

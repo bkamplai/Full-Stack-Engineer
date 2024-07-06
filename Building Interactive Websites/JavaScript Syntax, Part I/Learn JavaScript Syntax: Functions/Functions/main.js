798ce09e7df97cdd26767aa5821ee4652d120b04
bkamplai <sterypti@gmail.com>
2024-07-06 12:58:12 -0600

    Function Expressions

--- Building Interactive Websites/JavaScript Syntax, Part I/Learn JavaScript Syntax: Functions/Functions/main.js
+++ Building Interactive Websites/JavaScript Syntax, Part I/Learn JavaScript Syntax: Functions/Functions/main.js
@@ -39,3 +39,14 @@ function costOfMonitors(rows, columns) {
 
 const totalCost = costOfMonitors(5, 4);
 console.log(totalCost);
+
+// Function Expressions
+const plantNeedsWater = function (day) {
+    if (day === 'Wednesday') {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+console.log(plantNeedsWater('Tuesday'));

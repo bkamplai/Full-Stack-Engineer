e9b984bc2ccdcbc7bc1a0c40fd68319ba3a936a6
bkamplai <sterypti@gmail.com>
2024-07-05 20:29:36 -0600

    Ternary Operator

--- Building Interactive Websites/JavaScript Syntax, Part I/Learn JavaScript Syntax: Conditionals/Conditional Statements/main.js
+++ Building Interactive Websites/JavaScript Syntax, Part I/Learn JavaScript Syntax: Conditionals/Conditional Statements/main.js
@@ -1,40 +1,64 @@
+// If Statement
 let sale = true;
 sale = false;
+
 if (sale) {
     console.log('Time to buy!');
-} else {
+} else { // If...Else Statements
     console.log('Time to wait for a sale.');
 }
 
+// Comparison Operators
 let hungerLevel = 7;
+
 if (hungerLevel > 7) {
     console.log('Time to eat!');
 } else {
     console.log('We can eat later!');
 }
 
+// Logical Operators
 let mood = 'sleepy';
 let tirednessLevel = 6;
+
 if (mood === 'sleepy' && tirednessLevel > 8) {
     console.log('time to sleep');
 } else {
     console.log('not bed time yet');
 }
 
+// Truthy and Falsy
 let wordCount = 1;
+
 if (wordCount) {
     console.log("Great! You've started your work!");
 } else {
     console.log('Better get to work!');
 }
+
 let favoritePhrase = '';
+
 if (favoritePhrase) {
     console.log("This string doesn't seem to be empty.");
 } else {
     console.log('This string is definitely empty.');
 }
 
+// Truthy and Falsy Assignment
 let tool = 'marker';
 // Use short circuit evaluation to assign  writingUtensil variable below:
 let writingUtensil = tool || 'pen'
 console.log(`The ${writingUtensil} is mightier than the sword.`);
+
+// Ternary Operator
+let isLocked = false;
+
+isLocked ? console.log('You will need a key to open the door.') : console.log('You will not need a key to open the door.');
+
+let isCorrect = true;
+
+isCorrect ? console.log('Correct!') : console.log('Incorrect!');
+
+favoritePhrase = 'Love That!';
+
+favoritePhrase === 'Love That!' ? console.log('I love that!') : console.log("I don't love that!");

b9d259539bef93e9bd9f23c65355a7a3be2ebcac
bkamplai <sterypti@gmail.com>
2024-07-06 12:51:05 -0600

    Return

--- Building Interactive Websites/JavaScript Syntax, Part I/Learn JavaScript Syntax: Functions/Functions/main.js
+++ Building Interactive Websites/JavaScript Syntax, Part I/Learn JavaScript Syntax: Functions/Functions/main.js
@@ -23,3 +23,11 @@ function makeShoppingList(item1 = 'milk', item2 = 'bread', item3 = 'eggs') {
     console.log(`Remember to buy ${item2}`);
     console.log(`Remember to buy ${item3}`);
 }
+
+// Return
+function monitorCount(rows, columns) {
+    return rows * columns;
+}
+
+const numOfMonitors = monitorCount(5, 4);
+console.log(numOfMonitors);

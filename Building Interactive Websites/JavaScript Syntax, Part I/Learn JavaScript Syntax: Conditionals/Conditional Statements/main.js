db401e1914f78ca473bb833cd50cc4a8a37e4563
bkamplai <sterypti@gmail.com>
2024-07-05 20:38:07 -0600

    The switch keyword

--- Building Interactive Websites/JavaScript Syntax, Part I/Learn JavaScript Syntax: Conditionals/Conditional Statements/main.js
+++ Building Interactive Websites/JavaScript Syntax, Part I/Learn JavaScript Syntax: Conditionals/Conditional Statements/main.js
@@ -69,12 +69,29 @@ let season = 'summer';
 if (season === 'spring') {
     console.log('It\'s spring! The trees are budding!');
 } else if (season === 'winter') {
-    console.log('It\'s winter! Everything is covered in snow.')
+    console.log('It\'s winter! Everything is covered in snow.');
 } else if (season === 'fall') {
-    console.log('It\'s fall! Leaves are falling!')
+    console.log('It\'s fall! Leaves are falling!');
 } else if (season === 'summer') {
-    console.log('It\'s sunny and warm because it\'s summer!')
+    console.log('It\'s sunny and warm because it\'s summer!');
 } else {
     console.log('Invalid season.');
 }
 
+// The switch keyword
+let athleteFinalPosition = 'first place';
+
+switch (athleteFinalPosition) {
+    case 'first place':
+        console.log('You get the gold medal!');
+        break;
+    case 'second place':
+        console.log('You get the silver medal!');
+        break;
+    case 'third place':
+        console.log('You get the bronze medal!');
+        break;
+    default:
+        console.log('No medal awarded.');
+        break;
+}

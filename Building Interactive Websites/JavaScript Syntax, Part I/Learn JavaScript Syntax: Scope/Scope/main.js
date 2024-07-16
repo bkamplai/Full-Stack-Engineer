0f0d49d8245f0f5ffeda597935f7b724b94eaa4e
bkamplai <sterypti@gmail.com>
2024-07-16 17:33:48 -0600

    Global Scope

--- Building Interactive Websites/JavaScript Syntax, Part I/Learn JavaScript Syntax: Scope/Scope/main.js
+++ Building Interactive Websites/JavaScript Syntax, Part I/Learn JavaScript Syntax: Scope/Scope/main.js
@@ -1,6 +1,18 @@
+// Blocks and Scope
 const city = 'New York City';
 function logCitySkyline() {
     let skyscraper = 'Empire State Building';
     return 'The stars over the ' + skyscraper + ' in ' + city;
 }
 console.log(logCitySkyline())
+
+// Global Scope
+const satellite = 'The Moon';
+const galaxy = 'The Milky Way';
+const stars = 'North Star';
+
+function callMyNightSky() {
+    return 'Night Sky: ' + satellite + ', ' + stars + ', and ' + galaxy;
+}
+
+console.log(callMyNightSky());

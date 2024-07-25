c8d5283c90701b21a0111193e55a61bc76b06b68
Brandon Kamplain <sterypti@gmail.com>
2024-07-24 18:51:13 -0600

    finalGrade()

--- Building Interactive Websites/JavaScript Syntax, Part I/Practice JavaScript Syntax: Variables, Data Types, Conditionals, Functions/main.js
+++ Building Interactive Websites/JavaScript Syntax, Part I/Practice JavaScript Syntax: Variables, Data Types, Conditionals, Functions/main.js
@@ -37,3 +37,24 @@ function lifePhase(age) {
         return 'This is not a valid age';
     }
 }
+
+// finalGrade()
+function finalGrade(num1, num2, num3) {
+    if ((num1 < 0 || num1 > 100) || (num2 < 0 || num2 > 100) || (num3 < 0 || num3 > 100)) {
+        return 'You have entered an invalid grade.';
+    }
+
+    const average = (num1 + num2 + num3) / 3;
+
+    if (average >= 0 && average <= 59) {
+        return 'F';
+    } else if (average >= 60 && average <= 69) {
+        return 'D';
+    } else if (average >= 70 && average <= 79) {
+        return 'C';
+    } else if (average >= 80 && average <= 89) {
+        return 'B';
+    } else if (average >= 90 && average <= 100) {
+        return 'A';
+    }
+}

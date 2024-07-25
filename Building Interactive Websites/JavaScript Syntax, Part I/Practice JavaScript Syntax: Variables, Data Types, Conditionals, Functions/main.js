e90aab294d6a8cdb4e4869ec208d00692bee7ff8
Brandon Kamplain <sterypti@gmail.com>
2024-07-24 20:09:26 -0600

    calculateWeight()

--- Building Interactive Websites/JavaScript Syntax, Part I/Practice JavaScript Syntax: Variables, Data Types, Conditionals, Functions/main.js
+++ Building Interactive Websites/JavaScript Syntax, Part I/Practice JavaScript Syntax: Variables, Data Types, Conditionals, Functions/main.js
@@ -70,3 +70,21 @@ const rollTheDice = () => {
     let die2 = Math.floor(Math.random() * 6) + 1;
     return die1 + die2;
 }
+
+// calculateWeight()
+function calculateWeight(earthWeight, planet) {
+    switch (planet) {
+        case 'Mercury':
+            return earthWeight * 0.378;
+        case 'Venus':
+            return earthWeight * 0.907;
+        case 'Mars':
+            return earthWeight * 0.377;
+        case 'Jupiter':
+            return earthWeight * 2.36;
+        case 'Saturn':
+            return earthWeight * 0.916;
+        default:
+            return 'Invalid Planet Entry. Try: Mercury, Venus, Mars, Jupiter, or Saturn.';
+    }
+}

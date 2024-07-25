2a5ec9b854ca3dd684f8a30041c0baefc357b53f
Brandon Kamplain <sterypti@gmail.com>
2024-07-25 17:43:23 -0600

    Fix the broken code (round 2)!

--- Building Interactive Websites/JavaScript Syntax, Part I/Practice JavaScript Syntax: Variables, Data Types, Conditionals, Functions/main.js
+++ Building Interactive Websites/JavaScript Syntax, Part I/Practice JavaScript Syntax: Variables, Data Types, Conditionals, Functions/main.js
@@ -122,3 +122,33 @@ function howOld(age, year) {
         return `You were ${newAge} in the year ${year}`;
     }
 }
+
+// Fix the broken code (round 2)!
+const whatRelation = percentSharedDNA => {
+    if (percentSharedDNA === 100) {
+        return 'You are likely identical twins.'
+    }
+    if (percentSharedDNA > 34) {
+        return 'You are likely parent and child or full siblings.'
+    }
+    if (percentSharedDNA > 13) {
+        return 'You are likely grandparent and grandchild, aunt/uncle and niece/nephew, or half siblings.'
+    }
+    if (percentSharedDNA > 5) {
+        return 'You are likely 1st cousins.'
+    }
+    if (percentSharedDNA > 2) {
+        return 'You are likely 2nd cousins.'
+    }
+    if (percentSharedDNA > 0) {
+        return 'You are likely 3rd cousins'
+    }
+    return 'You are likely not related.'
+}
+
+console.log(whatRelation(34))
+// Should print 'You are likely grandparent and grandchild, aunt/uncle and niece/nephew, or half siblings.'
+
+console.log(whatRelation(3))
+// Should print 'You are likely 2nd cousins.'
+

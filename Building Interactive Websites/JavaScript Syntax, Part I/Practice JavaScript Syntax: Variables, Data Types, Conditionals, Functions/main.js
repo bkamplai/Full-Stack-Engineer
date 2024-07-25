24c2b3220ee553d844dbc16db4d09b17a9dd861d
Brandon Kamplain <sterypti@gmail.com>
2024-07-24 18:29:25 -0600

    canIVote()

--- Building Interactive Websites/JavaScript Syntax, Part I/Practice JavaScript Syntax: Variables, Data Types, Conditionals, Functions/main.js
+++ Building Interactive Websites/JavaScript Syntax, Part I/Practice JavaScript Syntax: Variables, Data Types, Conditionals, Functions/main.js
@@ -2,3 +2,12 @@
 function greetWorld() {
     return 'Hello, World!';
 }
+
+// canIVote()
+function canIVote(age) {
+    if (age >= 18) {
+        return true;
+    } else {
+        return false;
+    }
+}

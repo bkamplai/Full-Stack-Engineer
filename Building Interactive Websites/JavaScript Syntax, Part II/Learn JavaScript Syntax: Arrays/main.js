862d78559c270dcd93c7d57851f048b6ac60063e
Brandon Kamplain <sterypti@gmail.com>
2024-07-28 16:12:25 -0600

    The .length property

--- Building Interactive Websites/JavaScript Syntax, Part II/Learn JavaScript Syntax: Arrays/main.js
+++ Building Interactive Websites/JavaScript Syntax, Part II/Learn JavaScript Syntax: Arrays/main.js
@@ -25,3 +25,7 @@ console.log(condiments);  // Prints: [ 'Mayo' ]
 
 utensils[3] = 'Spoon';
 console.log(utensils);  // Prints: [ 'Fork', 'Knife', 'Chopsticks', 'Spoon' ]
+
+// The .length property
+const objectives = ['Learn a new language', 'Read 52 books', 'Run a marathon'];
+console.log(objectives.length);  // Prints: 3

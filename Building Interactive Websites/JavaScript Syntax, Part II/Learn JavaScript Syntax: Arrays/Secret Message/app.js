44fb8233c11ba4fa547a5d9741706bb8c32ce5e2
Brandon Kamplain <sterypti@gmail.com>
2024-07-28 16:56:52 -0600

    Secret Message

---
+++ Building Interactive Websites/JavaScript Syntax, Part II/Learn JavaScript Syntax: Arrays/Secret Message/app.js
@@ -0,0 +1,9 @@
+let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'easily', 'the', 'first', 'time,', 'it', 'is', 'about', 'what', 'you', 'can', 'figure', 'out.', '-2015,', 'Chris', 'Pine,', 'Learn', 'JavaScript'];
+secretMessage.pop();
+console.log(secretMessage.length);
+secretMessage.push('to', 'Program');
+secretMessage[secretMessage.indexOf('easily')] = 'right';
+secretMessage.shift();
+secretMessage.unshift('Programming');
+secretMessage.splice(secretMessage.indexOf('get'), 5, 'know');
+console.log(secretMessage.join(' '));

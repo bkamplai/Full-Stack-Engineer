51f550b66cd20f7938b6e9f8b586a553e86fc4d0
Brandon Kamplain <sterypti@gmail.com>
2024-07-28 16:02:53 -0600

    Accessing Elements

--- Building Interactive Websites/JavaScript Syntax, Part II/Learn JavaScript Syntax: Arrays/main.js
+++ Building Interactive Websites/JavaScript Syntax, Part II/Learn JavaScript Syntax: Arrays/main.js
@@ -1,3 +1,10 @@
 // Create an Array
 const hobbies = ['programming', 'video games', 'board/card games'];
 console.log(hobbies);
+
+// Accessing Elements
+const famousSayings = ['Fortune favors the brave.', 'A joke is a very serious thing.', 'Where there is love there is life.'];
+const listItem = famousSayings[0];
+console.log(listItem);          // Prints: Fortune favors the brave.
+console.log(famousSayings[2]);  // Prints: Where there is love there is life.
+console.log(famousSayings[3]);  // Prints: undefined

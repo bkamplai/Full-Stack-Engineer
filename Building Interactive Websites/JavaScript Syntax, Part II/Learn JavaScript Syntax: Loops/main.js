2be9cad0a948368bf733f1090f2c49a693861ef8
Brandon Kamplain <sterypti@gmail.com>
2024-07-28 18:13:34 -0600

    Repeating Tasks Manually

---
+++ Building Interactive Websites/JavaScript Syntax, Part II/Learn JavaScript Syntax: Loops/main.js
@@ -0,0 +1,5 @@
+// Repeating Tasks Manually
+const vacationSpots = ['Ireland', 'Germany', 'United Kingdom'];
+console.log(vacationSpots[0]);
+console.log(vacationSpots[1]);
+console.log(vacationSpots[2]);

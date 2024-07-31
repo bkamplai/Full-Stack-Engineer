fc6f6866ef58f221e6c2f412d4150a999e256b03
Brandon Kamplain <sterypti@gmail.com>
2024-07-31 17:42:45 -0600

    The break Keyword

--- Building Interactive Websites/JavaScript Syntax, Part II/Learn JavaScript Syntax: Loops/main.js
+++ Building Interactive Websites/JavaScript Syntax, Part II/Learn JavaScript Syntax: Loops/main.js
@@ -48,3 +48,15 @@ do {
     cupsAdded++;
     console.log(cupsAdded + ' cup was added');
 } while (cupsAdded < cupsOfSugarNeeded);
+
+// The break Keyword
+const rapperArray = ['Lil\' Kim', 'Jay-Z', 'Notorious B.I.G.', 'Tupac'];
+
+for (let i = 0; i < rapperArray.length; i++) {
+    console.log(rapperArray[i]);
+    if (rapperArray[i] === 'Notorious B.I.G.') {
+        break;
+    }
+}
+
+console.log("And if you don't know, now you know.");

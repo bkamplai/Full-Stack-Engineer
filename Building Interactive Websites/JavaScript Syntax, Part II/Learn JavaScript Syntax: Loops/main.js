81c9e57f12e75a70fda0830c9cb1634d70dde600
Brandon Kamplain <sterypti@gmail.com>
2024-07-30 18:56:06 -0600

    The While Loop

--- Building Interactive Websites/JavaScript Syntax, Part II/Learn JavaScript Syntax: Loops/main.js
+++ Building Interactive Websites/JavaScript Syntax, Part II/Learn JavaScript Syntax: Loops/main.js
@@ -31,3 +31,11 @@ for (let i = 0; i < bobsFollowers.length; i++) {
         }
     }
 }
+
+// The While Loop
+const cards = ['diamond', 'spade', 'heart', 'club'];
+let currentCard;
+while (currentCard !== 'spade') {
+    currentCard = cards[Math.floor(Math.random() * 4)];
+    console.log(currentCard);
+}

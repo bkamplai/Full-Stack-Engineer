617c1510020d674b8d0ac158cc24b811486a1e93
Brandon Kamplain <sterypti@gmail.com>
2024-08-10 12:14:52 -0600

    Accessing Properties

--- Building Interactive Websites/JavaScript Syntax, Part II/Learn JavaScript Syntax: Objects/app.js
+++ Building Interactive Websites/JavaScript Syntax, Part II/Learn JavaScript Syntax: Objects/app.js
@@ -3,3 +3,15 @@ let fastership = {
     color: 'silver',
     'Fuel Type': 'Turbo Fuel',
 };
+
+// Accessing Properties
+let spaceship = {
+    homePlanet: 'Earth',
+    color: 'silver',
+    'Fuel Type': 'Turbo Fuel',
+    numCrew: 5,
+    flightPath: ['Venus', 'Mars', 'Saturn']
+};
+
+let crewCount = spaceship.numCrew;
+let planetArray = spaceship.flightPath;

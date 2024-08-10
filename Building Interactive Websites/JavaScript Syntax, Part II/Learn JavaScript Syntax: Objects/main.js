27eec1fe7bca55ccaf83306573bbc5b7ff1c719e
Brandon Kamplain <sterypti@gmail.com>
2024-08-10 15:47:33 -0600

    Getters

--- Building Interactive Websites/JavaScript Syntax, Part II/Learn JavaScript Syntax: Objects/main.js
+++ Building Interactive Websites/JavaScript Syntax, Part II/Learn JavaScript Syntax: Objects/main.js
@@ -22,11 +22,26 @@ robot2.checkEnergy();  // Prints: Energy is currently at 100%.
 // Privacy
 const robot3 = {
     _energyLevel: 100,
-    recharge(){
-      this._energyLevel += 30;
-      console.log(`Recharged! Energy is currently at ${this._energyLevel}%.`)
+    recharge() {
+        this._energyLevel += 30;
+        console.log(`Recharged! Energy is currently at ${this._energyLevel}%.`)
     }
-  };
-  
-  robot3._energyLevel = 'high';
-  robot3.recharge();  // Prints: Recharged! Energy is currently at high30%.
+};
+
+robot3._energyLevel = 'high';
+robot3.recharge();  // Prints: Recharged! Energy is currently at high30%.
+
+// Getters
+const robot4 = {
+    _model: '1E78V2',
+    _energyLevel: 100,
+    get energyLevel() {
+        if (typeof this._energyLevel === 'number') {
+            return `My current energy level is ${this._energyLevel}`;
+        } else {
+            return 'System malfunction: cannot retrieve energy level';
+        }
+    }
+};
+
+console.log(robot4.energyLevel);  // Prints: My current energy level is 100

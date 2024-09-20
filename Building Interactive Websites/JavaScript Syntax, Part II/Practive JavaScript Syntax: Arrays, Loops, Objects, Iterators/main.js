fbf73613417bdf6dc3a2275f71dcd597d5025d33
Brandon Kamplain <sterypti@gmail.com>
2024-09-20 10:21:40 -0600

    squareNums()

--- Building Interactive Websites/JavaScript Syntax, Part II/Practive JavaScript Syntax: Arrays, Loops, Objects, Iterators/main.js
+++ Building Interactive Websites/JavaScript Syntax, Part II/Practive JavaScript Syntax: Arrays, Loops, Objects, Iterators/main.js
@@ -78,3 +78,12 @@ const acceptEverything = (arr) => {
 }
 
 acceptEverything(veggies);
+
+// squareNums()
+const numbers1 = [2, 7, 9, 171, 52, 33, 14];
+
+const toSquare = num => num * num;
+
+const squareNums = arr => arr.map(toSquare);
+
+console.log(squareNums(numbers1));

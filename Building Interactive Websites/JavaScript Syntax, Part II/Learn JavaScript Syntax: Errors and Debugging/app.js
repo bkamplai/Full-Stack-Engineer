1e59306ea66af7f5f0b45884f0f7766a2cae090f
Brandon Kamplain <sterypti@gmail.com>
2024-09-20 09:40:50 -0600

    Stack Overflow

--- Building Interactive Websites/JavaScript Syntax, Part II/Learn JavaScript Syntax: Errors and Debugging/app.js
+++ Building Interactive Websites/JavaScript Syntax, Part II/Learn JavaScript Syntax: Errors and Debugging/app.js
@@ -82,3 +82,15 @@ function doubleString(string) {
 
 // Should return 'echoecho'
 console.log("doubleString('echo') returns: " + doubleString('echo'));
+
+// Stack Overflow
+// Returns whether or not the provided string contains a substring of "cake" in it.
+function containsCake(string) {
+    return string.includes('cake');
+}
+
+// Should return true
+console.log("containsCake('I think cake is my soul mate.') returns: " + containsCake('I think cake is my soul mate.'));
+
+// Should return false
+console.log("containsCake('Pie is certainly the coolest dessert.') returns: " + containsCake('Pie is certainly the coolest dessert.'));

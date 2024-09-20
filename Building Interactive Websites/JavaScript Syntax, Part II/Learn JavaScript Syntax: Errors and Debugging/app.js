adc65a89debc93afb2db1062ab63a163c9c34166
Brandon Kamplain <sterypti@gmail.com>
2024-09-20 09:35:28 -0600

    Finding Documentation

--- Building Interactive Websites/JavaScript Syntax, Part II/Learn JavaScript Syntax: Errors and Debugging/app.js
+++ Building Interactive Websites/JavaScript Syntax, Part II/Learn JavaScript Syntax: Errors and Debugging/app.js
@@ -73,3 +73,12 @@ console.log("getLaterFirstLetter('zebra', 'aardvark') returns : " + getLaterFirs
 
 // Should return null
 console.log("getLaterFirstLetter('astro', 'afar') returns: " + getLaterFirstLetter('astro', 'afar'));
+
+// Finding Documentation
+// Link to String.repeat() documentation: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/repeat
+function doubleString(string) {
+    return string.repeat(2);
+}
+
+// Should return 'echoecho'
+console.log("doubleString('echo') returns: " + doubleString('echo'));

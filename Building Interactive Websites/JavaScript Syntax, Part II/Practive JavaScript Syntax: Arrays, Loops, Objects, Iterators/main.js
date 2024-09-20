ed13fba3209c01e006c887e0c608290dab285c4c
Brandon Kamplain <sterypti@gmail.com>
2024-09-20 10:24:09 -0600

    sortYears()

--- Building Interactive Websites/JavaScript Syntax, Part II/Practive JavaScript Syntax: Arrays, Loops, Objects, Iterators/main.js
+++ Building Interactive Websites/JavaScript Syntax, Part II/Practive JavaScript Syntax: Arrays, Loops, Objects, Iterators/main.js
@@ -95,3 +95,11 @@ const greetings = ['hello', 'hi', 'heya', 'oi', 'hey', 'yo'];
 
 console.log(shoutGreetings(greetings));
 // Should print [ 'HELLO!', 'HI!', 'HEYA!', 'OI!', 'HEY!', 'YO!' ]
+
+// sortYears()
+const sortYears = arr => arr.sort((a, b) => b - a);
+
+const years = [1970, 1999, 1951, 1982, 1963, 2011, 2018, 1922]
+
+console.log(sortYears(years));
+// Should print [ 2018, 2011, 1999, 1982, 1970, 1963, 1951, 1922 ]

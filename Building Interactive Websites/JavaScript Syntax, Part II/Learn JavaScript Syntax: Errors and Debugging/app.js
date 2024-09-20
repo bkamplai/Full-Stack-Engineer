effc1e2e3b1bc88b6765e6c828783bc987e1af7e
Brandon Kamplain <sterypti@gmail.com>
2024-09-20 09:29:40 -0600

    Debugging with console.log()

--- Building Interactive Websites/JavaScript Syntax, Part II/Learn JavaScript Syntax: Errors and Debugging/app.js
+++ Building Interactive Websites/JavaScript Syntax, Part II/Learn JavaScript Syntax: Errors and Debugging/app.js
@@ -47,3 +47,29 @@ console.log("capitalizeASingleWord('hey') returns: " + capitalizeASingleWord('he
 
 // Should return: null
 console.log("capitalizeASingleWord('hey ho') returns: " + capitalizeASingleWord('hey ho'));
+
+// Debugging with console.log()
+// Returns the string whose first letter is later in the alphabet. If both first letters are equal, returns null.
+function getLaterFirstLetter(string1, string2) {
+    // console.log(`string1: ${string1}, string2: ${string2}`);
+    const firstLetter1 = string1.charAt(0);
+    const firstLetter2 = string2.charAt(0);
+    // console.log(`firstLetter1: ${firstLetter1}, firstLetter2: ${firstLetter2}`);
+
+    if (firstLetter1 === firstLetter2) {
+        return null;
+    } else if (firstLetter1 < firstLetter2) {
+        return string2;
+    } else {
+        return string1;
+    }
+}
+
+// Should return "blueberry"
+console.log("getLaterFirstLetter('avocado', 'blueberry') returns: " + getLaterFirstLetter('avocado', 'blueberry'));
+
+// Should return "zebra"
+console.log("getLaterFirstLetter('zebra', 'aardvark') returns : " + getLaterFirstLetter('zebra', 'aardvark'));
+
+// Should return null
+console.log("getLaterFirstLetter('astro', 'afar') returns: " + getLaterFirstLetter('astro', 'afar'));

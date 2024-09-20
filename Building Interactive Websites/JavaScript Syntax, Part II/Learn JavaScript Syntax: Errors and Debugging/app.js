71ba286df61a5cefb058af0b7c197bbb08f21382
Brandon Kamplain <sterypti@gmail.com>
2024-09-19 19:53:13 -0600

    Debugging Errors

--- Building Interactive Websites/JavaScript Syntax, Part II/Learn JavaScript Syntax: Errors and Debugging/app.js
+++ Building Interactive Websites/JavaScript Syntax, Part II/Learn JavaScript Syntax: Errors and Debugging/app.js
@@ -1,3 +1,4 @@
+// JavaScript Error Types
 myVariable++;
 // 1 - What type of error will be thrown on the line above: Reference Error
 
@@ -7,3 +8,20 @@ myString.substring(0);
 
 const myRandomNumber; = Math.random();
 // 3 - What type of error will be thrown on the line above: Syntax Error
+
+// Debugging Errors
+function isSumBigger(number1, number2, total) {
+    const sum = number1 + number2;
+
+    if (sum > total) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// Should return true
+console.log('isSumBigger(1, 3, 2) returns: ' + isSumBigger(1, 3, 2));
+
+// Should return false
+console.log('isSumBigger(1, 3, 5) returns: ' + isSumBigger(1, 3, 5));

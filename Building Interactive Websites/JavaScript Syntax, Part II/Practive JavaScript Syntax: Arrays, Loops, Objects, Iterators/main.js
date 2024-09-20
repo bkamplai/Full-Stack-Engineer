9e20d0fc8d606d585a897f132853deb70d69e1c5
Brandon Kamplain <sterypti@gmail.com>
2024-09-20 10:06:16 -0600

    greetAliens()

--- Building Interactive Websites/JavaScript Syntax, Part II/Practive JavaScript Syntax: Arrays, Loops, Objects, Iterators/main.js
+++ Building Interactive Websites/JavaScript Syntax, Part II/Practive JavaScript Syntax: Arrays, Loops, Objects, Iterators/main.js
@@ -11,3 +11,14 @@ const sentence = ['sense.', 'make', 'all', 'will', 'This'];
 
 console.log(reverseArray(sentence));
 // Should print ['This', 'will', 'all', 'make', 'sense.'];
+
+// greetAliens()
+function greetAliens(aliens) {
+    for (let i = 0; i < aliens.length; i++) {
+        console.log(`Oh powerful ${aliens[i]}, we humans offer our unconditional surrender!`);
+    }
+}
+
+const aliens = ["Blorgous", "Glamyx", "Wegord", "SpaceKing"];
+
+greetAliens(aliens);

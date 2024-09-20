a2de9a04bfb69beb9e41ebb68412ca9feb892477
Brandon Kamplain <sterypti@gmail.com>
2024-09-19 19:49:34 -0600

    JavaScript Error Types

---
+++ Building Interactive Websites/JavaScript Syntax, Part II/Learn JavaScript Syntax: Errors and Debugging/app.js
@@ -0,0 +1,9 @@
+myVariable++;
+// 1 - What type of error will be thrown on the line above: Reference Error
+
+const myString = 42;
+myString.substring(0);
+// 2 - What type of error will be thrown on the line above: Type Error
+
+const myRandomNumber; = Math.random();
+// 3 - What type of error will be thrown on the line above: Syntax Error

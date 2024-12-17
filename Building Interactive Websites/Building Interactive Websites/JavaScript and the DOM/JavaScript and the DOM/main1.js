1b0f211c92d2929a6ede388bb8d1dd781b9712e0
Brandon Kamplain <sterypti@gmail.com>
2024-12-16 21:26:30 -0700

    Select and Modify Elements

---
+++ Building Interactive Websites/Building Interactive Websites/JavaScript and the DOM/JavaScript and the DOM/main1.js
@@ -0,0 +1,4 @@
+document.querySelector('h1').innerHTML = 'Most popular Harry Potter characters';
+document.getElementById('fourth').innerHTML = 'Professor Snape';
+document.getElementsByClassName('slytherin')[0].innerHTML = 'Salazar Slytherin';
+document.getElementsByTagName('li')[0].innerHTML = 'Dobby';

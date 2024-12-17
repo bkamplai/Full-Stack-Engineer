0537517ac32f5b9ca5aa21adc81f26df41860801
Brandon Kamplain <sterypti@gmail.com>
2024-12-16 21:47:43 -0700

    Create and Insert Elements

---
+++ Building Interactive Websites/Building Interactive Websites/JavaScript and the DOM/JavaScript and the DOM/main4.js
@@ -0,0 +1,4 @@
+let newAttraction = document.createElement('li');
+newAttraction.id = 'vespa';
+newAttraction.innerHTML = 'Rent a Vespa';
+document.getElementById('italy-attractions').appendChild(newAttraction);

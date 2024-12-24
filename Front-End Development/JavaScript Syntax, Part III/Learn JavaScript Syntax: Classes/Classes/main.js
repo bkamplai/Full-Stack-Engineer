5691a2cb6b238a00a16f6cecea394eca41d3f22e
Brandon Kamplain <sterypti@gmail.com>
2024-12-24 16:07:59 -0700

    Constructor

--- Front-End Development/JavaScript Syntax, Part III/Learn JavaScript Syntax: Classes/Classes/main.js
+++ Front-End Development/JavaScript Syntax, Part III/Learn JavaScript Syntax: Classes/Classes/main.js
@@ -23,3 +23,11 @@ console.log(halley.behavior); // Print behavior value to console
 halley.incrementBehavior(); // Add one to behavior
 console.log(halley.name); // Print name value to console
 console.log(halley.behavior); // Print behavior value to console
+
+// Constructor
+class Surgeon {
+    constructor(name, department) {
+        this.name = name;
+        this.department = department;
+    }
+}

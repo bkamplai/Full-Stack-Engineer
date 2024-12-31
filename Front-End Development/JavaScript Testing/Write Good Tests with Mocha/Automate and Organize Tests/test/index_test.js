b4e67ed136a5206110ab4e1b7a38c96482e2ed9f
Brandon Kamplain <sterypti@gmail.com>
2024-12-30 19:26:16 -0700

    assert

--- Front-End Development/JavaScript Testing/Write Good Tests with Mocha/Automate and Organize Tests/test/index_test.js
+++ Front-End Development/JavaScript Testing/Write Good Tests with Mocha/Automate and Organize Tests/test/index_test.js
@@ -1,5 +1,17 @@
+// describe and it blocks
 describe('Math', () => {
     describe('.min', () => {
         it('returns the argument with the lowest value', () => { });
+    });
+});
+
+// assert
+// import assert here
+const assert = require('assert');
+
+describe('+', () => {
+    it('returns the sum of its arguments', () => {
+        // Write assertion here
+        assert.ok(3 + 4 === 7);
     });
 });

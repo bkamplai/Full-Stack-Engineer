92e381301d46acefe24dc6c77c94b867306ce954
Brandon Kamplain <sterypti@gmail.com>
2024-12-30 19:31:22 -0700

    Setup, Exercise, and Verify

--- Front-End Development/JavaScript Testing/Write Good Tests with Mocha/Automate and Organize Tests/test/index_test.js
+++ Front-End Development/JavaScript Testing/Write Good Tests with Mocha/Automate and Organize Tests/test/index_test.js
@@ -15,3 +15,28 @@ describe('+', () => {
         assert.ok(3 + 4 === 7);
     });
 });
+
+// Setup, Exercise, and Verify
+
+// Naive approach
+describe('.pop', () => {
+    it('returns the last element in the array [naive]', () => {
+        assert.ok(['padawan', 'knight'].pop() === 'knight');
+    });
+});
+
+// 3 phase approach
+describe('.pop', () => {
+    it('returns the last element in the array [3phase]', () => {
+        // Setup
+        const knightString = 'knight';
+        const jediPath = ['padawan', knightString];
+
+        // Exercise
+        const popped = jediPath.pop();
+
+        // Verify
+        assert.ok(popped === knightString);
+    });
+});
+

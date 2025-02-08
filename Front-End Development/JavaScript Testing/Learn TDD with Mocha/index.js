5f162f268629dba63133b76b945998a8ce2c8c14
Brandon Kamplain <sterypti@gmail.com>
2025-02-08 14:10:56 -0700

    Refactor II

--- Front-End Development/JavaScript Testing/Learn TDD with Mocha/index.js
+++ Front-End Development/JavaScript Testing/Learn TDD with Mocha/index.js
@@ -1,13 +1,8 @@
 const Calculate = {
     sum(inputArray) {
-        let totalSum = 0;
-        const inputArrayLength = inputArray.length;
-
-        for (let i = 0; i < inputArrayLength; i++) {
-            totalSum += inputArray[i];
-        }
-
-        return totalSum;
+        return inputArray.reduce((sum, value) => {
+            return sum + value;
+        });
     }
 };
 

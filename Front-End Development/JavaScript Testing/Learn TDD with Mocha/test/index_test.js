86019e1782fd827c12ae52bdc9a48c558e77cafb
Brandon Kamplain <sterypti@gmail.com>
2025-02-08 13:42:41 -0700

    Getting Into The Red I

--- Front-End Development/JavaScript Testing/Learn TDD with Mocha/test/index_test.js
+++ Front-End Development/JavaScript Testing/Learn TDD with Mocha/test/index_test.js
@@ -1,8 +1,11 @@
 const assert = require('assert');
+const Calculate = require('../index.js')
 
 describe('Calculate', () => {
     describe('.sum', () => {
         it('returns the sum of an array of numbers', () => {
-        })
-    })
-})
+            // Code here
+            assert.strictEqual(Calculate.sum([1, 2, 3]), 6);
+        });
+    });
+});

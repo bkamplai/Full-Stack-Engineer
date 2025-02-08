d947400b0a938b6b6a18eadf66751f3fb2bd8b78
Brandon Kamplain <sterypti@gmail.com>
2025-02-08 12:08:50 -0700

    assert.deepStrictEqual I

--- Front-End Development/JavaScript Testing/Write Good Tests with Mocha/Write Expressive Tests/index_test.js
+++ Front-End Development/JavaScript Testing/Write Good Tests with Mocha/Write Expressive Tests/index_test.js
@@ -15,3 +15,17 @@ describe('-', () => {
         assert.strictEqual(result, expected);
     });
 });
+
+describe('+', () => {
+    it('returns the sum of two values', () => {
+        // Setup
+        let expected = { a: 3, b: 4, result: 7 };
+        let sum = { a: 3, b: 4 };
+
+        // Exercise
+        sum.result = sum.a + sum.b;
+
+        // Verify
+        assert.deepStrictEqual(sum, expected);
+    });
+});

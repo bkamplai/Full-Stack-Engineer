5cfd339be030797c6b958d1a5611fefe03031766
Brandon Kamplain <sterypti@gmail.com>
2025-05-12 12:28:59 -0600

    Handling Dependent Promises

---
+++ Front-End Development/Async JavaScript and HTTP Requests/Learn JavaScript Syntax: Async-Await/app2.js
@@ -0,0 +1,12 @@
+const { shopForBeans, soakTheBeans, cookTheBeans } = require('./library2.js');
+
+// Write your code below:
+async function makeBeans() {
+    let type = await shopForBeans();
+    let isSoft = await soakTheBeans(type);
+    let dinner = await cookTheBeans(isSoft);
+
+    console.log(dinner);
+}
+
+makeBeans();

23ce26f3724722b715b58136b4c15a0a8e20fc78
Brandon Kamplain <sterypti@gmail.com>
2025-05-12 15:35:16 -0600

    Making an async POST Request

--- Front-End Development/Async JavaScript and HTTP Requests/Learn JavaScript Syntax: Requests/bytesize/public/main.js
+++ Front-End Development/Async JavaScript and HTTP Requests/Learn JavaScript Syntax: Requests/bytesize/public/main.js
@@ -8,24 +8,25 @@ const shortenButton = document.querySelector('#shorten');
 const responseField = document.querySelector('#responseField');
 
 // Asynchronous functions
-const shortenUrl = () => {
+const shortenUrl = async () => {
     const urlToShorten = inputField.value;
     const data = JSON.stringify({ destination: urlToShorten });
-    fetch(url, {
-        method: 'POST',
-        headers: {
-            'Content-type': 'application/json',
-            'apikey': apiKey
-        },
-        body: data
-    }).then(response => {
-        if (response.ok) return response.json();
-        throw new Error('Request failed!');
-    }, networkError => {
-        console.log(networkError.message);
-    }).then(jsonResponse => {
-        renderResponse(jsonResponse);
-    });
+    try {
+        const response = await fetch(url, {
+            method: 'POST',
+            body: data,
+            headers: {
+                'Content-type': 'application/json',
+                'apikey': apiKey
+            }
+        });
+        if (response.ok) {
+            const jsonResponse = await response.json();
+            renderResponse(jsonResponse);
+        }
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 // Clear page and call Asynchronous functions

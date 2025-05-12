7c13582f16cf478654a8ccb5a019f8ac5c79d2c9
Brandon Kamplain <sterypti@gmail.com>
2025-05-12 15:07:55 -0600

    Handling a POST Request

--- Front-End Development/Async JavaScript and HTTP Requests/Learn JavaScript Syntax: Requests/bytesize/public/main.js
+++ Front-End Development/Async JavaScript and HTTP Requests/Learn JavaScript Syntax: Requests/bytesize/public/main.js
@@ -18,6 +18,13 @@ const shortenUrl = () => {
             'apikey': apiKey
         },
         body: data
+    }).then(response => {
+        if (response.ok) return response.json();
+        throw new Error('Request failed!');
+    }, networkError => {
+        console.log(networkError.message);
+    }).then(jsonResponse => {
+        renderResponse(jsonResponse);
     });
 }
 

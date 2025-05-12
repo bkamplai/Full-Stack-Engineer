4a985ace121943311649f1a0a6726b638105eb8e
Brandon Kamplain <sterypti@gmail.com>
2025-05-12 15:30:10 -0600

    Making an async GET Request

--- Front-End Development/Async JavaScript and HTTP Requests/Learn JavaScript Syntax: Requests/Word Smith/public/main.js
+++ Front-End Development/Async JavaScript and HTTP Requests/Learn JavaScript Syntax: Requests/Word Smith/public/main.js
@@ -1,36 +1,34 @@
 // Information to reach API
-const url = 'https://api.datamuse.com/words?sl=';
+const url = 'https://api.datamuse.com/words?';
+const queryParams = 'rel_jja=';
 
-// Selects page elements
+// Selecting page elements
 const inputField = document.querySelector('#input');
 const submit = document.querySelector('#submit');
 const responseField = document.querySelector('#responseField');
 
 // Asynchronous function
-const getSuggestions = () => {
+const getSuggestions = async () => {
     const wordQuery = inputField.value;
-    const endpoint = url + wordQuery;
-
-    fetch(endpoint, { cache: 'no-cache' })
-        .then(response => {
-            if (response.ok) return response.json();
-            throw new Error('Request failed!');
-        }, networkError => {
-            console.log(networkError.message);
-        })
-        .then(jsonResponse => {
-            // renderRawResponse(jsonResponse);
+    const endpoint = url + queryParams + wordQuery;
+    try {
+        const response = await fetch(endpoint, { cache: 'no-cache' });
+        if (response.ok) {
+            const jsonResponse = await response.json();
             renderResponse(jsonResponse);
-        });
+        }
+    } catch (error) {
+        console.log(error);
+    }
 }
 
-// Clears previous results and display results to webpage
+// Clear previous results and display results to webpage
 const displaySuggestions = (event) => {
     event.preventDefault();
     while (responseField.firstChild) {
-        responseField.removeChild(responseField.firstChild);
+        responseField.removeChild(responseField.firstChild)
     }
     getSuggestions();
-};
+}
 
 submit.addEventListener('click', displaySuggestions);

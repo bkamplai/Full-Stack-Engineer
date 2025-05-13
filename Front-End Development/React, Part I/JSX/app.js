4fe2d3f5d2908b07290096fbba6b9eab4f5baa8a
Brandon Kamplain <sterypti@gmail.com>
2025-05-12 21:57:20 -0600

    JSX Outer Elements

--- Front-End Development/React, Part I/JSX/app.js
+++ Front-End Development/React, Part I/JSX/app.js
@@ -17,3 +17,15 @@ const myDiv = (
         <h1>Hello world</h1>
     </div>
 );
+
+// JSX Outer Elements
+const blog = (
+    <div>
+        <img src="pics/192940u73.jpg" />
+        <h1>Welcome to Dan's Blog!</h1>
+        <article>
+            Wow I had the tastiest sandwich today. I <strong>literally</strong> almost
+            freaked out.
+        </article>
+    </div>
+);

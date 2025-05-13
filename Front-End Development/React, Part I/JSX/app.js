00b18e0ede1511ef1a7e9e058096175b1aa0e04f
Brandon Kamplain <sterypti@gmail.com>
2025-05-12 21:58:59 -0600

    Rendering JSX

--- Front-End Development/React, Part I/JSX/app.js
+++ Front-End Development/React, Part I/JSX/app.js
@@ -29,3 +29,12 @@ const blog = (
         </article>
     </div>
 );
+
+// Rendering JSX
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+// Copy code here:
+const container = document.getElementById('app');
+const root = createRoot(container);
+root.render(<h1>Hello world</h1>);

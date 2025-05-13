2b77cd2fd5950ee7b298940ef2e6c4c8078766c8
Brandon Kamplain <sterypti@gmail.com>
2025-05-12 21:54:00 -0600

    Attributes In JSX

--- Front-End Development/React, Part I/JSX/app.js
+++ Front-End Development/React, Part I/JSX/app.js
@@ -6,3 +6,7 @@ const h1 = <h1>Hello world</h1>;
 
 // JSX Elements And Their Surroundings
 const myArticle = <article></article>;
+
+// Attributes In JSX
+const p1 = <p id='large'>foo</p>;
+const p2 = <p id='small'>bar</p>;

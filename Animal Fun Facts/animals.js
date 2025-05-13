bfeb262af40af5c4765c1c3774f006af11901d60
Brandon Kamplain <sterypti@gmail.com>
2025-05-13 15:43:24 -0600

    fix: update image paths in animals.js and app.js for consistency

--- Animal Fun Facts/animals.js
+++ Animal Fun Facts/animals.js
@@ -1,14 +1,14 @@
 export const animals = {
     dolphin: {
-        image: 'Animal%20Fun%20Facts/images/dolphin.jpg',
+        image: 'Animal Fun Facts/images/dolphin.jpg',
         facts: ['Dolphins have been shown to give distinct names to each other!', 'Dolphins are known to display their own culture!', 'Dolphins have two stomachs!']
     },
     lobster: {
-        image: 'Animal%20Fun%20Facts/images/lobster.jpg',
+        image: 'Animal Fun Facts/images/lobster.jpg',
         facts: ['Lobsters taste with their legs!', 'Lobsters chew with their stomachs!', 'Lobsters can live as long as 100 years.']
     },
     starfish: {
-        image: 'Animal%20Fun%20Facts/images/starfish.jpg',
+        image: 'Animal Fun Facts/images/starfish.jpg',
         facts: ['Starfish can have up to 40 arms!', 'Starfish have no brain and no blood!', 'Starfish can regenerate their own arms!']
     }
 };

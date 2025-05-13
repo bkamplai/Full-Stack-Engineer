b90b4e2136743509d67abfdc396a8031870f7646
Brandon Kamplain <sterypti@gmail.com>
2025-05-13 16:57:32 -0600

    Using and Rendering a Component

--- Front-End Development/React, Part I/React Components/index.js
+++ Front-End Development/React, Part I/React Components/index.js
@@ -1,3 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import MyComponent from './App';
+
+ReactDOM.createRoot(document.getElementById('app')).render(<MyComponent />);

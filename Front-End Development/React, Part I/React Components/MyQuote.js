13ff780543e2d93116de9556603ead7d0218525d
Brandon Kamplain <sterypti@gmail.com>
2025-05-13 17:02:43 -0600

    Use Multiline JSX in a Component

---
+++ Front-End Development/React, Part I/React Components/MyQuote.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function MyQuote() {
+    return (
+        <blockquote>
+            <p>
+                What is important now is to recover our senses.
+            </p>
+            <cite>
+                <a target="_blank"
+                    href="https://en.wikipedia.org/wiki/Susan_Sontag">
+                    Susan Sontag
+                </a>
+            </cite>
+        </blockquote>
+    );
+};
+
+export default MyQuote;

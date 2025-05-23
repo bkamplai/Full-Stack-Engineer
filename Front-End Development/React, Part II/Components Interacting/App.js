fa9ae16a554d5998a1974b90cd9231105e3866db
Brandon Kamplain <sterypti@gmail.com>
2025-05-23 17:57:19 -0600

    Access a Component's props

---
+++ Front-End Development/React, Part II/Components Interacting/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import PropsDisplayer from './PropsDisplayer';
+
+function App() {
+    return <PropsDisplayer />;
+}
+
+export default App;

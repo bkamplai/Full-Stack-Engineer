7089549056c2e3c00b2fe02ab1da55ea805d913b
Brandon Kamplain <sterypti@gmail.com>
2025-05-23 18:08:26 -0600

    Render a Component's props

--- Front-End Development/React, Part II/Components Interacting/App.js
+++ Front-End Development/React, Part II/Components Interacting/App.js
@@ -2,9 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import PropsDisplayer from './PropsDisplayer';
+import Product from './Product';
 
 function App() {
-    return <PropsDisplayer myProp="Hello"/>;
+    return (
+        <>
+            <PropsDisplayer myProp="Hello" />
+            <Product name="A Minecraft Movie" price={19.99} rating="5.8/10" />
+        </>
+    );
 }
 
 export default App;

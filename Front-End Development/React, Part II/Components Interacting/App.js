18b724a8b133f5624741023164d24a116d45d3cf
Brandon Kamplain <sterypti@gmail.com>
2025-05-23 18:15:05 -0600

    Pass props From Component To Component

--- Front-End Development/React, Part II/Components Interacting/App.js
+++ Front-End Development/React, Part II/Components Interacting/App.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom';
 
 import PropsDisplayer from './PropsDisplayer';
 import Product from './Product';
+import Player from './Player';
 
 function App() {
     return (
         <>
             <PropsDisplayer myProp="Hello" />
             <Product name="A Minecraft Movie" price={19.99} rating="5.8/10" />
+            <Player songName='Bloodline (with Jelly Roll)' artist='Alex Warren' />
         </>
     );
 }

1ac8fd95b8067e9836e2b32a7aac6ea7019f3a72
Brandon Kamplain <sterypti@gmail.com>
2025-05-23 19:41:43 -0600

    Put an Event Handler in a Function Component

---
+++ Front-End Development/React, Part II/Components Interacting/Talker.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Button from "./Button";
+
+function Talker() {
+    function talk() {
+        let speech = "";
+        for (let i = 0; i < 10000; i++) {
+            speech += "blah ";
+        }
+        alert(speech);
+    }
+    return <Button />;
+}
+
+export default Talker;

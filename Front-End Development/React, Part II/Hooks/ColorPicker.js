c0795c9599ddbfd983dbb9169ac73fbbcaebb6d2
Brandon Kamplain <sterypti@gmail.com>
2025-05-31 17:27:50 -0600

    Initialize State

--- Front-End Development/React, Part II/Hooks/ColorPicker.js
+++ Front-End Development/React, Part II/Hooks/ColorPicker.js
@@ -1,26 +1,23 @@
 // import the default export and the named export `useState` from the 'react' library
 import React, { useState } from 'react';
 
+const colorNames = ['Aquamarine', 'BlueViolet', 'Chartreuse', 'CornflowerBlue', 'Thistle', 'SpringGreen', 'SaddleBrown', 'PapayaWhip', 'MistyRose'];
+
 export default function ColorPicker() {
     // call useState and assign its return values to `color` and `setColor`
-    const [color, setColor] = useState();
+    const [color, setColor] = useState('Tomato');
     const divStyle = { backgroundColor: color };
 
     return (
         <div style={divStyle}>
             <p>The color is {color}</p>
-            <button onClick={() => setColor('Aquamarine')}>
-                Aquamarine
-            </button>
-            <button onClick={() => setColor('BlueViolet')}>
-                BlueViolet
-            </button>
-            <button onClick={() => setColor('Chartreuse')}>
-                Chartreuse
-            </button>
-            <button onClick={() => setColor('CornflowerBlue')}>
-                CornflowerBlue
-            </button>
+            {colorNames.map((colorName) => (
+                <button
+                    onClick={() => setColor(colorName)}
+                    key={colorName}>
+                    {colorName}
+                </button>
+            ))}
         </div>
     );
 }
